c30b714166f09eb3b770a296c04a5191d47259ef
student <student@fsg-pc55.fsg-preetz.org>
2022-12-02 10:18:32 +0100

    aufgabe 3 1, am anfang verbessert

--- aufgaben/src/aufgaben/Aufgabe_3.java
+++ aufgaben/src/aufgaben/Aufgabe_3.java
@@ -6,7 +6,8 @@ public class Aufgabe_3 {
 		// 1,1,2,4,7,11,16,22,29,37,....
 
 		int b = 0;
-		for (int i = 1; i <= 60; i = b + i) {
+		System.out.print("1,");
+		for (int i = 1; i < 60 ; i = b + i) {
 			b = b + 1;
 			System.out.print(i);
 			if (i != 56)
